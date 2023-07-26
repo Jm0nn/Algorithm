adc0e3dfd7a98b35c2411fb98281b1bbdc0e63cc
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-07-26 23:17:50 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,24 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+// 세개의 자연수의 곱에서 0부터 9까지 각각의 숫자가 몇 개씩 나타나는지 구하는 문제
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// a, b, c 입력
 		int a = Integer.parseInt(br.readLine());
 		int b = Integer.parseInt(br.readLine());
 		int c = Integer.parseInt(br.readLine());
 		
-		for (int i = 0; i < 10; i++) {
-			int count = 0;
-			int n = a * b * c;
-			while (n > 0) {
-				if (n % 10 == i)
-					count++;
-				n /= 10;
-			}
-			System.out.println(count);
+		int n = a * b * c; // n 계산
+		int[] digit = new int[10]; // 숫자 배열 생성
+		
+		while (n > 0) { // n이 0보다 클 동안 반복
+			digit[n % 10]++; // n의 일의 자리 숫자 한 개 증가
+			
+			n /= 10; // n을 10으로 나눠서 반복
 		}
-        
+		
+		for (int i = 0; i < 10; i++) // 배열 출력
+			System.out.println(digit[i]);
+		
 		br.close();
 	}
 }
