56f3ccda84d5d8fa874df449708db53889e77ba9
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-07-29 14:12:41 +0900

    [Silver IV] Title: 큐, Time: 196 ms, Memory: 19684 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -12,13 +12,15 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine()); // 명령의 횟수
 		Queue<Integer> queue = new LinkedList<>(); // 정수를 저장하는 큐
+		int num = 0; // 큐에 마지막으로 추가 할 정수
 
 		while (N-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine()); // 명령 입력
 
 			switch (st.nextToken()) {
 			case "push": // 큐에 정수 추가
-				queue.add(Integer.parseInt(st.nextToken()));
+				num = Integer.parseInt(st.nextToken()); // 큐에 가장 마지막에 추가된 정수
+				queue.add(num);
 				break;
 
 			case "pop": // 큐에서 정수 빼고 그 수 출력
@@ -49,10 +51,8 @@ public class Main {
 			case "back": // 큐의 가장 뒷 정수 출력
 				if (queue.isEmpty()) // 큐가 비어있다면
 					sb.append(-1).append('\n'); // -1 출력
-				else { // 큐의 가장 뒷 요소를 보기 위해 큐를 배열의 형태로 바꾼 후 가장 마지막 요소를 출력
-					Integer[] arr = new Integer[queue.size()];
-					queue.toArray(arr);
-					sb.append(arr[queue.size() - 1]).append('\n');
+				else { // 가장 마지막에 큐에 넣은 정수 출력
+					sb.append(num).append('\n');
 				}
 				break;
 			}
