290f2ab3e7dbeca9d7471e39ba32867ad91284a5
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-07-30 19:47:56 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 132 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine()); // 별 모양 층수
+
+		int blank = N - 1; // 한 행의 앞 부분 빈 공간 갯수
+		for (int i = 0; i < N; i++) {
+			for (int j = blank--; j > 0; j--) // 한 행의 앞 부분 빈 공간 출력
+				sb.append(' ');
+
+			for (int j = 0; j < i * 2 + 1; j++) {
+				if (j % 2 == 1) // 빈 공간 출력
+					sb.append(' ');
+				else // 별 출력
+					sb.append('*');
+			}
+
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+
+		br.close();
+	}
+}
