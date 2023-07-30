4403dc03e2e2bacb4794e572b3dddfd6e40e61f4
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-07-30 22:46:17 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 136 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				sb.append('*');
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+
+		br.close();
+	}
+}
