1aae4625be3be12a7e5be77775d9493ef0f79bc5
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-07-31 22:34:34 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] step = new int[n + 1];
+		int[] dp = new int[n + 1];
+		
+		for (int i = 1; i <= n; i++)
+			step[i] = Integer.parseInt(br.readLine());
+		
+		dp[1] = step[1];
+		
+		if (n > 1)
+			dp[2] = step[1] + step[2];
+		
+		for (int i = 3; i <= n; i++)
+			dp[i] = Math.max(dp[i - 2], dp[i - 3] + step[i - 1]) + step[i];
+		
+		System.out.println(dp[n]);
+		
+		br.close();
+	}
+	
+}
