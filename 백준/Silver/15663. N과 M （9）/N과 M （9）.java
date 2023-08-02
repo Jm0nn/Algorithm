d6d0cade66ad625e4d71c91573addea99dbf8b11
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-02 14:33:20 +0900

    [Silver II] Title: N과 M (9), Time: 288 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -10,10 +10,9 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int N, M; // 출력할 자연수의 갯수, 배열의 크기
-	static int[] numArr; // 자연수 배열, 중복 불가
+	static int[] num = new int[10001]; // 자연수가 입력된 개수를 저장하는 배열
+	static int[] numArr; // 자연수 배열
 	static int[] printArr; // 출력할 배열
-	static boolean[] visitNum; // 자연수 방문 확인 배열
-	static List<String> visitArr = new ArrayList<>(); // 중복 수열을 확인하기 위한 리스트
 
 	static StringBuilder sb = new StringBuilder();
 
@@ -23,15 +22,11 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		numArr = new int[N];
 		printArr = new int[M];
-		visitNum = new boolean[N];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++)
-			numArr[i] = Integer.parseInt(st.nextToken());
-
-		Arrays.sort(numArr); // 자연수 배열 정렬
+			num[Integer.parseInt(st.nextToken())]++;
 
 		dfs(0); // 모든 수열을 확인하기 위한 깊이 우선 탐색
 
@@ -43,32 +38,20 @@ public class Main {
 	// 깊이 우선 탐색
 	static void dfs(int depth) {
 		if (depth == M) { // 출력할 배열의 크기에 도달했다면
-			StringBuilder s = new StringBuilder();
-
-			// 배열 문자열로 생성
-			for (int i : printArr)
-				s.append(i).append(' ');
-			s.append('\n');
-
-			String arr = s.toString();
-
-			// 기존에 출력한 배열 중 새로운 배열과 같은 배열이 있는지 확인
-			if (!visitArr.contains(arr)) {
-				visitArr.add(arr); // 새로운 배열 추가
-				sb.append(arr); // 출력
-			}
-
+			for (int i : printArr) // 배열 출력
+				sb.append(i).append(' ');
+			sb.append('\n');
 			return;
 		}
 
-		for (int i = 0; i < N; i++) { // 0부터 N-1까지 반복문 사용
-			if (visitNum[i]) // 방문했다면 탐색 넘어감
+		for (int i = 1; i < 10001; i++) { // 1부터 10000까지 수 중 입력받은 수 탐색
+			if (num[i] < 1) // 입력받지 않은 수라면 탐색 넘어감
 				continue;
 
-			visitNum[i] = true; // 방문 확인
-			printArr[depth] = numArr[i]; // 배열에 해당 자연수 추가
+			num[i]--; // 방문 확인
+			printArr[depth] = i; // 배열에 해당 자연수 추가
 			dfs(depth + 1); // 다음 배열 탐색
-			visitNum[i] = false; // 방문 취소
+			num[i]++; // 방문 취소
 		}
 	}
 }
