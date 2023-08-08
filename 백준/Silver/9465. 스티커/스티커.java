0e360b54781cb3b290649d92595dfef08e8bc7a0
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-08 11:37:40 +0900

    [Silver I] Title: 스티커, Time: 756 ms, Memory: 116488 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -12,32 +12,22 @@ public class Main {
 
 		while (T-- > 0) {
 			int n = Integer.parseInt(br.readLine()); // 스티커 크기
-			int[][] arr = new int[3][n]; // 스티커 배열, (3, n) 배열은 스티커가 없는 배열
-			int[][] dp = new int[3][n]; // dp 배열
+			int[][] dp = new int[2][n + 1]; // dp 배열
 
+			// 초기 스티커의 점수 넣음
 			for (int i = 0; i < 2; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++)
-					arr[i][j] = Integer.parseInt(st.nextToken());
+				for (int j = 1; j <= n; j++)
+					dp[i][j] = Integer.parseInt(st.nextToken());
 			}
 
-			dp[0][0] = arr[0][0];
-			dp[1][0] = arr[1][0];
-
-			for (int i = 1; i < n; i++) {
-				for (int j = 0; j < 3; j++) {
-					for (int k = 0; k < 3; k++) {
-						if (j != k) // 같은 행, 같은 열의 스티커는 뗄 수 없음
-							dp[k][i] = Math.max(dp[k][i], dp[j][i - 1] + arr[k][i]);
-					}
-				}
+			// 같은 행, 열에 연속으로 스티커를 뗄 수 없고, 한 열에 스티커를 모두 떼지 않을 수도 있음
+			for (int i = 2; i <= n; i++) {
+				dp[0][i] += Math.max(dp[1][i - 1], dp[1][i - 2]);
+				dp[1][i] += Math.max(dp[0][i - 1], dp[0][i - 2]);
 			}
 
-			int max = 0;
-			for (int i = 0; i < 3; i++)
-				max = Math.max(max, dp[i][n - 1]);
-
-			sb.append(max).append('\n');
+			sb.append(Math.max(dp[0][n], dp[1][n])).append('\n');
 		}
 
 		System.out.println(sb);
