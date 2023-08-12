aed9d79cc2f1889f357a8993c55b213fca433825
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-12 16:18:54 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 136 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 범위 내에서 어떤 수의 제곱으로 나누어 떨어지지 않는 수의 갯수를 구하는 문제
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long min = Long.parseLong(st.nextToken()); // 범위 내 최솟값
+		long max = Long.parseLong(st.nextToken()); // 범위 내 최댓값
+
+		long maxSqrt = (long) Math.sqrt(max); // 최댓값의 제곱근
+		int range = (int) (max - min + 1); // 범위
+		boolean[] sqrtNum = new boolean[range]; // 제곱ㄴㄴ수이면 false
+
+		// 에라토스테네스의 체
+		for (long i = 2; i <= maxSqrt; i++) {
+			long square = i * i; // 인덱스의 제곱으로 나눔
+
+			// 최솟값이 인덱스의 제곱으로 나누어 떨어지는지 확인
+			// 나누어 떨어지면 해당 수부터 체크
+			// 나누어 떨어지지 않으면 해당 수 +1부터 체크
+			long start = min % square == 0 ? min / square : min / square + 1;
+
+			// 제곱ㄴㄴ수가 아닌 수를 true로 설정
+			for (long j = start; j * square <= max; j++)
+				sqrtNum[(int) (j * square - min)] = true;
+		}
+
+		int cnt = 0; // 제곱ㄴㄴ수의 개수
+		for (int i = 0; i < range; i++) {
+			if (!sqrtNum[i])
+				cnt++;
+		}
+
+		System.out.println(cnt);
+
+		br.close();
+	}
+}
