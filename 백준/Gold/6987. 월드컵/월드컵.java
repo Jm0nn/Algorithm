a73ee09356b2330b85e09b5763a260d48b904387
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-16 16:35:26 +0900

    [Gold IV] Title: 월드컵, Time: 80 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 월드컵 조별 예선 경기에서 결과가 가능한지 구하는 문제
 public class Main {
 
-	static int[][][] worldCup = new int[4][6][3];
+	static int[][][] score = new int[4][6][3]; // 승무패 배열: 4조, 6팀, 승무패 횟수
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,51 +15,52 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 6; j++) {
 				for (int k = 0; k < 3; k++) {
-					worldCup[i][j][k] = Integer.parseInt(st.nextToken());
+					score[i][j][k] = Integer.parseInt(st.nextToken());
 				}
 			}
 
 			if (recur(0, i, 0, 1))
-				sb.append(1);
+				sb.append(1); // 가능한 결과라면 1 출력
 			else
-				sb.append(0);
+				sb.append(0); // 불가능한 결과라면 0 출력
 			sb.append(' ');
 		}
 
 		System.out.println(sb);
 	}
 
+	// cnt: 경기 횟수, group: 그룹 번호, s1, s2: 경기할 두 팀
 	static boolean recur(int cnt, int group, int s1, int s2) {
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (worldCup[group][i][j] < 0)
-					return false;
-			}
-		}
-
-		if (cnt == 15) {
+		if (cnt == 15) { // 15회 경기를 했을 때
 			for (int i = 0; i < 6; i++) {
 				for (int j = 0; j < 3; j++) {
-					if (worldCup[group][i][j] != 0)
-						return false;
+					if (score[group][i][j] != 0)
+						return false; // 결과가 입력과 맞지 않으면 false
 				}
 			}
 
-			return true;
+			return true; // 결과가 입력과 맞으면 true
 		}
 
-		for (int i = 0; i < 3; i++) {
-			worldCup[group][s1][i] -= 1;
-			worldCup[group][s2][2 - i] -= 1;
+		for (int i = 0; i < 3; i++) { // 두 팀의 승무패
+			// 각 팀의 승무패 결과 -1
+			score[group][s1][i] -= 1;
+			score[group][s2][2 - i] -= 1;
 
-			if (s2 == 5) {
-				if (recur(cnt + 1, group, s1 + 1, s1 + 2))
-					return true;
-			} else if (recur(cnt + 1, group, s1, s2 + 1))
-				return true;
+			// 결과가 음수가 되면 넘어감
+			if (score[group][s1][i] >= 0 && score[group][s2][2 - i] >= 0) {
+				if (s2 == 5) {
+					if (recur(cnt + 1, group, s1 + 1, s1 + 2))
+						return true;
+				} else {
+					if (recur(cnt + 1, group, s1, s2 + 1))
+						return true;
+				}
+			}
 
-			worldCup[group][s1][i] += 1;
-			worldCup[group][s2][2 - i] += 1;
+			// 각 팀의 승무패 결과 원복
+			score[group][s1][i] += 1;
+			score[group][s2][2 - i] += 1;
 		}
 
 		return false;
