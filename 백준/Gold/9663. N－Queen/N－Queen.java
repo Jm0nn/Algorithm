04aa3f7b7baa5e83cb4c30f4836e780ad1ddd9f6
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-16 10:13:14 +0900

    [Gold IV] Title: N-Queen, Time: 2744 ms, Memory: 12244 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -6,14 +6,16 @@ public class Main {
 
 	static int n, cnt; // Queen의 개수, 경우의 수
 	static int[] queen; // Queen의 배치 배열
-	static boolean[] visit; // 해당 index의 행에 Queen이 놓이면 true
+	static boolean[] putRow, putDia1, putDia2; // 행, 대각선에 Queen이 놓이면 true
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		n = Integer.parseInt(br.readLine());
 		queen = new int[n];
-		visit = new boolean[n];
+		putRow = new boolean[n];
+		putDia1 = new boolean[n * 2 - 1];
+		putDia2 = new boolean[n * 2 - 1];
 
 		putQueen(0); // 백트래킹으로 Queen의 배치 경우의 수를 계산
 
@@ -27,26 +29,14 @@ public class Main {
 		}
 
 		for (int i = 0; i < n; i++) {
-			if (visit[i]) // 현재 행에 이미 Queen이 놓여있으면 다음 탐색
+			// 행과 대각선에 Queen이 놓여있으면 다음 행 탐색
+			if (putRow[i] || putDia1[depth + i] || putDia2[depth - i + n - 1])
 				continue;
 
 			queen[depth] = i;
-			if (isValid(depth)) { // 현재 위치가 유효한 위치인지 확인
-				visit[i] = true;
-				putQueen(depth + 1); // 다음 행 배치
-				visit[i] = false;
-			}
+			putRow[i] = putDia1[depth + i] = putDia2[depth - i + n - 1] = true;
+			putQueen(depth + 1); // 다음 열 배치
+			putRow[i] = putDia1[depth + i] = putDia2[depth - i + n - 1] = false;
 		}
-
-	}
-
-	static boolean isValid(int col) {
-		// 첫번째 행부터 현재 행까지 유효성 검사
-		for (int i = 0; i < col; i++)
-			// 대각선에 Queen이 놓이면 false
-			if (col - i == Math.abs(queen[col] - queen[i]))
-				return false;
-
-		return true; // 유효하면 true
 	}
 }
