fa8bb813422dc2fe705efb3b600da1e3f24b2023
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-18 21:20:02 +0900

    [Bronze III] Title: 주식 투자, Time: 320 ms, Memory: 33896 KB -BaekjoonHub

---
+++ 백준/Bronze/13416. 주식 투자/주식 투자.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			int sum = 0;
+			while (n-- > 0) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				int max = Math.max(a, Math.max(b, c));
+				if (max > 0)
+					sum += max;
+			}
+			sb.append(sum).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
