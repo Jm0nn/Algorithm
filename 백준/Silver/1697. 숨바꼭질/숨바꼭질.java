7ea036a47e52f56256d7d1920d7b57cc50747478
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-18 14:20:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 108 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,20 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 수빈이가 동생을 만나는데 걸리는 시간을 구하는 문제
 public class Main {
 
-	static final int MAX_VALUE = 100_001;	// 좌표 한계
+	static final int MAX_VALUE = 100_001; // 좌표 한계
 
-	static int N, K;	// 수빈이와 동생의 좌표
-	static boolean[] visit = new boolean[MAX_VALUE];	// 방문 정보
+	static int N, K; // 수빈이와 동생의 좌표
+	static boolean[] visit = new boolean[MAX_VALUE]; // 방문 정보
 
-	static class State {	// 현재 수빈이의 상태를 나타내는 클래스
-		int pos;	// 현재 좌표
-		int cnt;	// 현재 걸린 시간
+	static class State { // 현재 수빈이의 상태를 나타내는 클래스
+		int pos; // 현재 좌표
+		int cnt; // 현재 걸린 시간
 
 		public State(int pos, int cnt) {
 			this.pos = pos;
@@ -23,53 +22,53 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		if (N < K)	// 동생이 수빈이보다 앞에 있을 경우
-			System.out.println(bfs());	// 걸린 시간의 최솟값을 구하기 위한 넓이 우선 탐색
-		else	// 수빈이가 동생보다 앞에 있을경우 뒤로 한 칸씩만 이동 가능
-			System.out.println(N - K);	// 거리 차 = 걸린 시간
+		if (N < K) // 동생이 수빈이보다 앞에 있을 경우
+			System.out.println(bfs()); // 걸린 시간의 최솟값을 구하기 위한 넓이 우선 탐색
+		else // 수빈이가 동생보다 앞에 있을경우 뒤로 한 칸씩만 이동 가능
+			System.out.println(N - K); // 거리 차 = 걸린 시간
 
 		br.close();
 	}
 
 	// 넓이 우선 탐색
 	static int bfs() {
-		Queue<State> queue = new LinkedList<>();
+		Queue<State> queue = new ArrayDeque<>();
 		queue.add(new State(N, 0));
 		visit[N] = true;
 
 		while (!queue.isEmpty()) {
 			State cur = queue.poll();
 
+			// 동생을 만난 경우 걸린 시간 반환
+			if (cur.pos == K)
+				return cur.cnt;
+
 			// 이동 방법 세가지
 			for (int i = 0; i < 3; i++) {
-				int npos = cur.pos;	// 현재 좌표
-				int ncnt = cur.cnt + 1;	// 현재 걸린 시간
-				
+				int npos = cur.pos; // 현재 좌표
+				int ncnt = cur.cnt + 1; // 현재 걸린 시간
+
 				switch (i) {
-				case 0:	// 한 칸 뒤로 이동
+				case 0: // 한 칸 뒤로 이동
 					npos--;
 					break;
-				case 1:	// 한 칸 앞으로 이동
+				case 1: // 한 칸 앞으로 이동
 					npos++;
 					break;
-				case 2:	// x2의 위치로 순간이동
+				case 2: // x2의 위치로 순간이동
 					npos *= 2;
 					break;
 				}
-				
-				if (npos == K)	// 동생을 만날 경우 걸린 시간 반환
-					return ncnt;
-				
+
 				// 배열을 벗어나지 않고 방문하지 않은 경우
 				if ((0 <= npos && npos < MAX_VALUE) && !visit[npos]) {
-					visit[npos] = true;	// 방문 확인 후 큐에 추가
+					visit[npos] = true; // 방문 확인 후 큐에 추가
 					queue.add(new State(npos, ncnt));
 				}
 			}
@@ -77,4 +76,5 @@ public class Main {
 
 		return 0;
 	}
+
 }
