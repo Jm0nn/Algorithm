0be0025c772f10b94fbc6181ce990bb238dcb707
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-21 17:45:59 +0900

    [Gold V] Title: 적록색약, Time: 96 ms, Memory: 12320 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -12,29 +12,17 @@ public class Main {
 	static boolean[][] normal;
 	static boolean[][] blind;
 
-	static class Pos {
-		int r, c;
-
-		Pos(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		n = Integer.parseInt(br.readLine());
 
-		paint = new char[n][n];
+		paint = new char[n][];
 		normal = new boolean[n][n];
 		blind = new boolean[n][n];
 
-		for (int i = 0; i < n; i++) {
-			String s = br.readLine();
-			for (int j = 0; j < n; j++)
-				paint[i][j] = s.charAt(j);
-		}
+		for (int i = 0; i < n; i++)
+			paint[i] = br.readLine().toCharArray();
 
 		int nCnt = 0;
 		int bCnt = 0;
@@ -53,20 +41,20 @@ public class Main {
 			}
 		}
 
-		System.out.println(nCnt + " " + bCnt);
+		System.out.println(new StringBuilder().append(nCnt).append(' ').append(bCnt));
 	}
 
 	static void bfsNormal(int r, int c, char color) {
-		Queue<Pos> queue = new ArrayDeque<>();
-		queue.offer(new Pos(r, c));
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] { r, c });
 		normal[r][c] = true;
 
 		while (!queue.isEmpty()) {
-			Pos cur = queue.poll();
+			int[] cur = queue.poll();
 
 			for (int d = 0; d < 4; d++) {
-				int nr = cur.r + deltas[d][0];
-				int nc = cur.c + deltas[d][1];
+				int nr = cur[0] + deltas[d][0];
+				int nc = cur[1] + deltas[d][1];
 
 				if (0 > nr || nr >= n || 0 > nc || nc >= n)
 					continue;
@@ -74,23 +62,23 @@ public class Main {
 				if (normal[nr][nc] || paint[nr][nc] != color)
 					continue;
 
-				queue.offer(new Pos(nr, nc));
+				queue.offer(new int[] { nr, nc });
 				normal[nr][nc] = true;
 			}
 		}
 	}
 
 	static void bfsBlind(int r, int c, char color) {
-		Queue<Pos> queue = new ArrayDeque<>();
-		queue.offer(new Pos(r, c));
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] { r, c });
 		blind[r][c] = true;
 
 		while (!queue.isEmpty()) {
-			Pos cur = queue.poll();
+			int[] cur = queue.poll();
 
 			for (int d = 0; d < 4; d++) {
-				int nr = cur.r + deltas[d][0];
-				int nc = cur.c + deltas[d][1];
+				int nr = cur[0] + deltas[d][0];
+				int nc = cur[1] + deltas[d][1];
 
 				if (0 > nr || nr >= n || 0 > nc || nc >= n)
 					continue;
@@ -106,7 +94,7 @@ public class Main {
 						continue;
 				}
 
-				queue.offer(new Pos(nr, nc));
+				queue.offer(new int[] { nr, nc });
 				blind[nr][nc] = true;
 			}
 		}
