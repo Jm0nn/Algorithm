5cd5e3969a6f067176035e289b28acf2af25eed2
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-29 17:35:55 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12056 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,28 +3,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static final int RED = 0;
-	static final int GREEN = 1;
-	static final int BLUE = 2;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[][] rgb = new int[n + 1][3];
+		int[][] rgb = new int[n][3];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 3; j++)
 				rgb[i][j] = Integer.parseInt(st.nextToken());
+
+			if (i == 0)
+				continue;
+
+			rgb[i][0] += Math.min(rgb[i - 1][1], rgb[i - 1][2]);
+			rgb[i][1] += Math.min(rgb[i - 1][0], rgb[i - 1][2]);
+			rgb[i][2] += Math.min(rgb[i - 1][0], rgb[i - 1][1]);
 		}
 
-		for (int i = 1; i <= n; i++) {
-			rgb[i][RED] += Math.min(rgb[i - 1][GREEN], rgb[i - 1][BLUE]);
-			rgb[i][GREEN] += Math.min(rgb[i - 1][RED], rgb[i - 1][BLUE]);
-			rgb[i][BLUE] += Math.min(rgb[i - 1][GREEN], rgb[i - 1][RED]);
-		}
-
-		System.out.println(Math.min(rgb[n][RED], Math.min(rgb[n][GREEN], rgb[n][BLUE])));
+		System.out.println(Math.min(rgb[n - 1][0], Math.min(rgb[n - 1][1], rgb[n - 1][2])));
 	}
 }
