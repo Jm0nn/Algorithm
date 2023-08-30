afc7b14993fbe319b95c600363d2b217b9b4ca96
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-30 11:23:33 +0900

    [Gold V] Title: 동전 1, Time: 92 ms, Memory: 11872 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,153 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		FastIO fio = new FastIO();
+		int n = fio.nextInt();
+		int k = fio.nextInt();
+		int[] coin = new int[n];
+		int[] dp = new int[k + 1];
+
+		for (int i = 0; i < n; i++)
+			coin[i] = fio.nextInt();
+
+		dp[0] = 1;
+
+		for (int c : coin)
+			for (int i = c; i <= k; i++)
+				dp[i] += dp[i - c];
+
+		fio.write(dp[k]);
+		fio.flush();
+		fio.close();
+	}
+
+	public static class FastIO {
+
+		private BufferedReader br;
+		private BufferedWriter bw;
+		private StringTokenizer st;
+
+		public FastIO() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+
+		public String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		public FastIO write(String str) {
+			try {
+				bw.write(str);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return this;
+		}
+
+		public FastIO write(char c) {
+			try {
+				bw.write(c);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return this;
+		}
+
+		public FastIO write(int i) {
+			try {
+				bw.write(Integer.toString(i));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return this;
+		}
+
+		public FastIO write(long l) {
+			try {
+				bw.write(Long.toString(l));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return this;
+		}
+
+		public FastIO write(double d) {
+			try {
+				bw.write(Double.toString(d));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return this;
+		}
+
+		public FastIO write(boolean b) {
+			try {
+				bw.write(Boolean.toString(b));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return this;
+		}
+
+		public FastIO newLine() {
+			try {
+				bw.write("\n");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return this;
+		}
+
+		public void flush() {
+			try {
+				bw.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			try {
+				br.close();
+				bw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
