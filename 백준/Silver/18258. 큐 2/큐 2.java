a4b050b006e6cc87d5ed75ad62468765d280100a
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-08-31 23:27:34 +0900

    [Silver IV] Title: 큐 2, Time: 888 ms, Memory: 322912 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,182 +1,50 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		FastIO fio = new FastIO();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = fio.nextInt();
+		int n = Integer.parseInt(br.readLine());
 		int[] queue = new int[n];
 
 		int head = 0, tail = 0;
 
+		StringBuilder sb = new StringBuilder();
+
 		while (n-- > 0) {
-			switch (fio.next()) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			switch (st.nextToken()) {
 			case "push":
-				queue[tail++] = fio.nextInt();
-				break;
+				queue[tail++] = Integer.parseInt(st.nextToken());
+				continue;
 
 			case "pop":
-				fio.write(head != tail ? queue[head++] : -1).newLine();
+				sb.append(head != tail ? queue[head++] : -1);
 				break;
 
 			case "size":
-				fio.write(tail - head).newLine();
+				sb.append(tail - head);
 				break;
 
 			case "empty":
-				fio.write(head == tail ? 1 : 0).newLine();
+				sb.append(head == tail ? 1 : 0);
 				break;
 
 			case "front":
-				fio.write(head != tail ? queue[head] : -1).newLine();
+				sb.append(head != tail ? queue[head] : -1);
 				break;
 
 			case "back":
-				fio.write(head != tail ? queue[tail - 1] : -1).newLine();
+				sb.append(head != tail ? queue[tail - 1] : -1);
 				break;
 			}
+			sb.append('\n');
 		}
 
-		fio.flush();
-		fio.close();
+		System.out.println(sb);
 	}
 
-	public static class FastIO {
-
-		private BufferedReader br;
-		private BufferedWriter bw;
-		private StringTokenizer st;
-
-		public FastIO() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-			bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		}
-
-		public String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
-
-		public int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		public long nextLong() {
-			return Long.parseLong(next());
-		}
-
-		public double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		public String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-
-		public FastIO write(String s) {
-			try {
-				bw.write(s);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public FastIO write(char c) {
-			try {
-				bw.write(c);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public FastIO write(int i) {
-			try {
-				bw.write(Integer.toString(i));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public FastIO write(long l) {
-			try {
-				bw.write(Long.toString(l));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public FastIO write(double d) {
-			try {
-				bw.write(Double.toString(d));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public FastIO write(boolean b) {
-			try {
-				bw.write(Boolean.toString(b));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public FastIO write(Object o) {
-			try {
-				bw.write(o.toString());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public FastIO newLine() {
-			try {
-				bw.newLine();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return this;
-		}
-
-		public void flush() {
-			try {
-				bw.flush();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		public void close() {
-			try {
-				br.close();
-				bw.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
 }
