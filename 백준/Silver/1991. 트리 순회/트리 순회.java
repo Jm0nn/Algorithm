ff1c3b5c1769cf1296958c03173408f8d523f439
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-09-02 18:00:17 +0900

    [Silver I] Title: 트리 순회, Time: 80 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Node {
+		char left, right;
+
+		Node(char left, char right) {
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	static Node[] tree = new Node[26];
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char node = st.nextToken().charAt(0);
+			char left = st.nextToken().charAt(0);
+			char right = st.nextToken().charAt(0);
+
+			tree[node - 'A'] = new Node(left, right);
+		}
+
+		pre('A');
+		sb.append('\n');
+		in('A');
+		sb.append('\n');
+		post('A');
+
+		System.out.println(sb);
+	}
+
+	static void pre(char c) {
+		if (c == '.')
+			return;
+		sb.append(c);
+		pre(tree[c - 'A'].left);
+		pre(tree[c - 'A'].right);
+	}
+
+	static void in(char c) {
+		if (c == '.')
+			return;
+		in(tree[c - 'A'].left);
+		sb.append(c);
+		in(tree[c - 'A'].right);
+	}
+
+	static void post(char c) {
+		if (c == '.')
+			return;
+		post(tree[c - 'A'].left);
+		post(tree[c - 'A'].right);
+		sb.append(c);
+	}
+
+}
