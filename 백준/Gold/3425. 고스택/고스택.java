4d24a58f723a440c08037f6840661cb8ce6010e2
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-09-07 00:29:11 +0900

    [Gold IV] Title: 고스택, Time: 272 ms, Memory: 67136 KB -BaekjoonHub

--- 백준/Gold/3425. 고스택/고스택.java
+++ 백준/Gold/3425. 고스택/고스택.java
@@ -2,33 +2,38 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
+// 고스택 프로그램을 사용하는 문제
 public class Main {
 
-	static final long MAX_NUM = 1_000_000_000L;
+	static final long MAX_NUM = 1_000_000_000L; // 고스택 내 수의 최댓값
 
+	// 고스택 클래스
 	static class GoStack {
-		long[] stack;
-		int size;
-		int top;
+		long[] stack; // 스택 배열
+		int top; // 스택 내 수의 개수 (스택의 가장 위에 저장된 수의 인덱스 + 1)
 
-		GoStack(int size, int start) {
-			this.size = size + 1;
-			stack = new long[size + 1];
+		// 처음에 스택에 start를 넣고 시작
+		GoStack(int start) {
+			stack = new long[1000];
 			stack[top++] = start;
 		}
 
+		// x를 스택의 가장 위에 저장
 		void num(int x) {
 			stack[top++] = x;
 		}
 
+		// 스택 가장 위의 수를 제거
+		// 스택에 수가 없으면 false
 		boolean pop() {
 			if (--top < 0)
 				return false;
 			return true;
 		}
 
+		// 스택 가장 위의 수의 부호를 바꿈
+		// 스택에 수가 없으면 false
 		boolean inv() {
 			if (top == 0)
 				return false;
@@ -36,6 +41,8 @@ public class Main {
 			return true;
 		}
 
+		// 스택의 가장 위의 수를 하나 더 저장
+		// 스택에 수가 없으면 false
 		boolean dup() {
 			if (top == 0)
 				return false;
@@ -43,6 +50,8 @@ public class Main {
 			return true;
 		}
 
+		// 스택의 가장 위 두 수의 위치를 바꿈
+		// 바꿀 수가 없으면 false
 		boolean swp() {
 			if (top < 2)
 				return false;
@@ -52,6 +61,8 @@ public class Main {
 			return true;
 		}
 
+		// 스택의 가장 위 두 수를 꺼내서 합을 저장
+		// 꺼낼 수가 없거나 저장한 수의 절댓값이 최댓값을 넘으면 false
 		boolean add() {
 			if (top < 2)
 				return false;
@@ -61,6 +72,8 @@ public class Main {
 			return true;
 		}
 
+		// 스택의 가장 위 두 수를 꺼내서 차를 저장
+		// 꺼낼 수가 없거나 저장한 수의 절댓값이 최댓값을 넘으면 false
 		boolean sub() {
 			if (top < 2)
 				return false;
@@ -70,6 +83,8 @@ public class Main {
 			return true;
 		}
 
+		// 스택의 가장 위 두 수를 꺼내서 곱을 저장
+		// 꺼낼 수가 없거나 저장한 수의 절댓값이 최댓값을 넘으면 false
 		boolean mul() {
 			if (top < 2)
 				return false;
@@ -79,6 +94,8 @@ public class Main {
 			return true;
 		}
 
+		// 스택의 가장 위 두 수를 꺼내서 나눗셈의 몫을 저장
+		// 꺼낼 수가 없거나 나누는 수가 0이면 false
 		boolean div() {
 			if (top < 2 || stack[top - 1] == 0)
 				return false;
@@ -86,6 +103,8 @@ public class Main {
 			return true;
 		}
 
+		// 스택의 가장 위 두 수를 꺼내서 나눗셈의 나머지를 저장
+		// 꺼낼 수가 없거나 나누는 수가 0이면 false
 		boolean mod() {
 			if (top < 2 || stack[top - 1] == 0)
 				return false;
@@ -93,6 +112,7 @@ public class Main {
 			return true;
 		}
 
+		// 스택에 남은 수를 출력
 		long result() {
 			return stack[0];
 		}
@@ -101,92 +121,77 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
 		program: while (true) {
-			List<String> cmdList = new ArrayList<>();
+			List<String> cmdList = new ArrayList<>(); // 커맨드 리스트
 
-			String s = "";
-			while (!(s = br.readLine()).equals("END")) {
-				if (s.equals("QUIT"))
+			String s = ""; // 커맨드 입력
+			// END가 나올 때 까지 입력
+			while ((s = br.readLine()).charAt(0) != 'E') {
+				// QUIT이 입력되면 프로그램 종료
+				if (s.charAt(0) == 'Q')
 					break program;
-				cmdList.add(s);
+				cmdList.add(s); // 입력받은 커맨드 리스트에 저장
 			}
 
+			// 연산을 수행할 횟수
 			int n = Integer.parseInt(br.readLine());
 			for (int i = 0; i < n; i++) {
-				int v = Integer.parseInt(br.readLine());
-				GoStack gs = new GoStack(cmdList.size(), v);
+				int v = Integer.parseInt(br.readLine()); // 연산을 수행할 수
+				GoStack gs = new GoStack(v); // 고스택
 
-				boolean isValid = true;
+				boolean isNotError = true; // 에러 여부
 
+				// 커맨드 리스트에 저장된 커맨드를 순서대로 실행
 				for (String cmd : cmdList) {
-					st = new StringTokenizer(cmd);
-					String cmd1 = st.nextToken();
-
-					switch (cmd1) {
-					case "NUM":
-						gs.num(Integer.parseInt(st.nextToken()));
-						break;
-
+					// 커맨드 실행
+					switch (cmd) {
 					case "POP":
-						if (!gs.pop())
-							isValid = false;
+						isNotError = gs.pop();
 						break;
-
 					case "INV":
-						if (!gs.inv())
-							isValid = false;
+						isNotError = gs.inv();
 						break;
-
 					case "DUP":
-						if (!gs.dup())
-							isValid = false;
+						isNotError = gs.dup();
 						break;
-
 					case "SWP":
-						if (!gs.swp())
-							isValid = false;
+						isNotError = gs.swp();
 						break;
-
 					case "ADD":
-						if (!gs.add())
-							isValid = false;
+						isNotError = gs.add();
 						break;
-
 					case "SUB":
-						if (!gs.sub())
-							isValid = false;
+						isNotError = gs.sub();
 						break;
-
 					case "MUL":
-						if (!gs.mul())
-							isValid = false;
+						isNotError = gs.mul();
 						break;
-
 					case "DIV":
-						if (!gs.div())
-							isValid = false;
+						isNotError = gs.div();
 						break;
-
 					case "MOD":
-						if (!gs.mod())
-							isValid = false;
+						isNotError = gs.mod();
 						break;
+					default:
+						gs.num(Integer.parseInt(cmd.substring(4)));
 					}
 
-					if (!isValid)
+					// 에러 발생 시 프로그램 종료
+					if (!isNotError)
 						break;
 				}
 
-				if (!isValid || gs.top != 1)
+				// 에러가 발생했거나 고스택에 남은 수가 한 개가 아니라면 에러 출력
+				// 프로그램이 정상적으로 종료되어 수가 한 개가 남았다면 해당 수 출력
+				if (!isNotError || gs.top != 1)
 					sb.append("ERROR\n");
 				else
 					sb.append(gs.result()).append('\n');
 			}
 
-			sb.append('\n');
-			br.readLine();
+			sb.append('\n'); // 공백 출력
+			br.readLine(); // 공백 입력
 		}
 
 		System.out.println(sb);
