a1360703113c735510ea4786d4f95908041cbeef
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-09-21 09:03:59 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 524 ms, Memory: 62636 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,22 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static class Node {
-		String key;
-		String value;
-		Node next;
-
-		public Node(String key, String value, Node next) {
-			this.key = key;
-			this.value = value;
-			this.next = next;
-		}
-	}
-
-	static final int HASH = 5381;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,7 +13,7 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 
-		Node[] hashMap = new Node[HASH];
+		Map<String, String> map = new HashMap<>();
 
 		for (int i = 0; i < n; ++i) {
 			st = new StringTokenizer(br.readLine());
@@ -33,46 +21,17 @@ public class Main {
 			String key = st.nextToken();
 			String value = st.nextToken();
 
-			int idx = getIndex(key);
-
-			hashMap[idx] = new Node(key, value, hashMap[idx]);
+			map.put(key, value);
 		}
 
 		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < m; ++i) {
 			String key = br.readLine();
-			int idx = getIndex(key);
-
-			for (Node cur = hashMap[idx]; cur != null; cur = cur.next) {
-				if (key.equals(cur.key)) {
-					sb.append(cur.value).append('\n');
-					break;
-				}
-			}
+			sb.append(map.get(key)).append('\n');
 		}
 
 		System.out.println(sb);
-	}
-
-	static int hashCode(String s) {
-		int hash = HASH;
-		int len = s.length();
-
-		for (int i = 0; i < len; ++i) {
-			hash = (hash << 5) + s.charAt(i);
-		}
-
-		if (hash < 0) {
-			hash *= -1;
-		}
-
-		return hash;
-	}
-
-	static int getIndex(String s) {
-		int hash = hashCode(s);
-		return hash % HASH;
 	}
 
 }
