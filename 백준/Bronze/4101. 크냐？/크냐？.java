31cfd19d027c8f0e196227b4b1b200b2484a3895
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-07 11:10:13 +0900

    [Bronze V] Title: 크냐?, Time: 84 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0)
+
+                break;
+
+            sb.append(a > b ? "Yes\n" : "No\n");
+
+        }
+
+        System.out.print(sb);
+
+    }
+
+}
