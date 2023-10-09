223b04ad362a3a6d5196b2496450e6c1300b0efc
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-09 20:53:06 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		boolean[] visit = new boolean[100001];
+		Queue<int[]> queue = new ArrayDeque<>();
+
+		visit[n] = true;
+		queue.offer(new int[] { n, 0 });
+
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			int x = cur[0];
+			int cnt = cur[1];
+
+			if (x == k) {
+				System.out.println(cnt);
+				break;
+			}
+
+			int t = x * 2;
+
+			if (t <= 100000 && !visit[t]) {
+				visit[t] = true;
+				queue.offer(new int[] { t, cnt });
+			}
+
+			int b = x - 1;
+
+			if (b >= 0 && !visit[b]) {
+				visit[b] = true;
+				queue.offer(new int[] { b, cnt + 1 });
+			}
+
+			int f = x + 1;
+
+			if (f <= 100000 && !visit[f]) {
+				visit[f] = true;
+				queue.offer(new int[] { f, cnt + 1 });
+			}
+
+		}
+	}
+
+}
