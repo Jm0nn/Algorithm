57734276fec40d414fb69ae739354121f7420a66
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-12 21:26:45 +0900

    [Gold III] Title: 별자리 만들기, Time: 96 ms, Memory: 12344 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -7,14 +7,13 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static class Node implements Comparable<Node> {
-		int no;
+		int from, to;
 		double dist;
-		Node next;
 
-		Node(int no, double dist, Node next) {
-			this.no = no;
+		Node(int from, int to, double dist) {
+			this.from = from;
+			this.to = to;
 			this.dist = dist;
-			this.next = next;
 		}
 
 		@Override
@@ -23,15 +22,21 @@ public class Main {
 		}
 	}
 
+	static int[] parents;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
 
 		double[][] star = new double[n][2];
-		Node[] graph = new Node[n];
+
+		parents = new int[n];
+		Queue<Node> pq = new PriorityQueue<>();
 
 		for (int i = 0; i < n; ++i) {
+			parents[i] = i;
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
 			star[i][0] = Double.parseDouble(st.nextToken());
@@ -42,33 +47,42 @@ public class Main {
 				double dy = Math.abs(star[i][1] - star[j][1]);
 				double dist = Math.sqrt(dx * dx + dy * dy);
 
-				graph[i] = new Node(j, dist, graph[i]);
-				graph[j] = new Node(i, dist, graph[j]);
+				pq.offer(new Node(i, j, dist));
 			}
 		}
 
-		boolean[] visit = new boolean[n];
-		Queue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(0, 0, null));
 		double sum = 0;
+		int cnt = 0;
 
 		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
-			int no = cur.no;
-			double dist = cur.dist;
+			Node node = pq.poll();
 
-			if (visit[no])
-				continue;
+			if (union(node.from, node.to)) {
+				sum += node.dist;
 
-			visit[no] = true;
-			sum += dist;
-
-			for (Node node = graph[no]; node != null; node = node.next) {
-				if (!visit[node.no])
-					pq.offer(node);
+				if (++cnt == n - 1)
+					break;
 			}
 		}
 
 		System.out.println(String.format("%.2f", sum));
+	}
+
+	static int find(int a) {
+		if (a == parents[a])
+			return a;
+
+		return parents[a] = find(parents[a]);
+	}
+
+	static boolean union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+
+		if (rootA == rootB)
+			return false;
+
+		parents[rootA] = rootB;
+		return true;
 	}
 }
