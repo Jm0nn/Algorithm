7b3947e804060edcd7f8cd9f46c25d7f14f2fd30
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-12 12:10:42 +0900

    [Gold II] Title: 피보나치 수 3, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static final long MOD = 1_000_000L;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long n = Long.parseLong(br.readLine());
+
+		long[][] res = fibo(n);
+
+		System.out.println(res[1][0]);
+	}
+
+	static long[][] fibo(long n) {
+		long[][] BASE = new long[][] { { 1L, 0L }, { 0L, 1L } };
+		long[][] Q = new long[][] { { 1L, 1L }, { 1L, 0L } };
+
+		while (n >= 1L) {
+			if (n % 2L == 1L)
+				BASE = mul(BASE, Q);
+			Q = mul(Q, Q);
+			n /= 2L;
+		}
+
+		return BASE;
+	}
+
+	static long[][] mul(long[][] m1, long[][] m2) {
+		long[][] res = new long[2][2];
+
+		for (int i = 0; i < 2; ++i) {
+			for (int j = 0; j < 2; ++j) {
+				for (int k = 0; k < 2; ++k) {
+					res[i][j] = (res[i][j] + (m1[i][k] * m2[k][j]) % MOD) % MOD;
+				}
+			}
+		}
+
+		return res;
+	}
+
+}
