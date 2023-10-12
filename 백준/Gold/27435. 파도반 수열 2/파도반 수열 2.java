30c0cdd590c6ab5212853e2cf3d3f189bdbe86ae
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-12 12:48:40 +0900

    [Gold I] Title: 파도반 수열 2, Time: 200 ms, Memory: 27824 KB -BaekjoonHub

---
+++ 백준/Gold/27435. 파도반 수열 2/파도반 수열 2.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static final long MOD = 998_244_353L;
+	static long[] p = new long[100];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			long n = Long.parseLong(br.readLine());
+			sb.append(pado(n)[2][1]).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	static long[][] pado(long n) {
+		long[][] BASE = new long[][] { { 1L, 0L, 0L }, { 0L, 1L, 0L }, { 0L, 0L, 1L } };
+		long[][] Q = new long[][] { { 0L, 1L, 0L }, { 0L, 0L, 1L }, { 1L, 1L, 0L } };
+
+		while (n > 0L) {
+			if (n % 2L == 1L)
+				BASE = mul(BASE, Q);
+			Q = mul(Q, Q);
+			n /= 2L;
+		}
+
+		return BASE;
+	}
+
+	static long[][] mul(long[][] m1, long[][] m2) {
+		long[][] res = new long[3][3];
+
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				for (int k = 0; k < 3; ++k) {
+					res[i][j] = (res[i][j] + (m1[i][k] * m2[k][j]) % MOD) % MOD;
+				}
+			}
+		}
+
+		return res;
+	}
+
+}
