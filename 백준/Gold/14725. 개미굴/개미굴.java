2c20d396be2adac058d94b01dcbb6aa88be03072
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-17 21:58:38 +0900

    [Gold III] Title: 개미굴, Time: 140 ms, Memory: 13568 KB -BaekjoonHub

---
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Node {
+		Map<String, Node> child;
+
+		Node() {
+			child = new HashMap<>();
+		}
+
+		void add(List<String> list) {
+			Node node = this;
+
+			for (String s : list) {
+				node.child.putIfAbsent(s, new Node());
+				node = node.child.get(s);
+			}
+		}
+
+		void print(Node node, int depth) {
+			Node cur = node;
+
+			if (cur.child.size() == 0)
+				return;
+
+			List<String> list = new ArrayList<>(cur.child.keySet());
+			Collections.sort(list);
+
+			for (String s : list) {
+				for (int i = 0; i < depth; ++i)
+					sb.append("--");
+				sb.append(s).append('\n');
+				print(cur.child.get(s), depth + 1);
+			}
+		}
+	}
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		Node trie = new Node();
+
+		for (int i = 0; i < n; ++i) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int k = Integer.parseInt(st.nextToken());
+			List<String> list = new ArrayList<>();
+
+			while (k-- > 0)
+				list.add(st.nextToken());
+
+			trie.add(list);
+		}
+
+		trie.print(trie, 0);
+
+		System.out.println(sb);
+	}
+
+}
