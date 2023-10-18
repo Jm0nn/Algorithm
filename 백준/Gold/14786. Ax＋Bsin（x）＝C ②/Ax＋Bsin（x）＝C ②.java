322a6ca35785540818cd3f7f165474b6e279c524
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-18 21:45:53 +0900

    [Gold III] Title: Ax+Bsin(x)=C ②, Time: 76 ms, Memory: 11656 KB -BaekjoonHub

---
+++ 백준/Gold/14786. Ax＋Bsin（x）＝C ②/Ax＋Bsin（x）＝C ②.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static final double ERROR = 0.000000001;
+
+	static int A, B, C;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		double left = 0;
+		double right = C * 2;
+
+		double mid = (left + right) / 2;
+
+		while (Math.abs(right - left) >= ERROR) {
+			if (f(mid) > 0)
+				right = mid;
+			else
+				left = mid;
+
+			mid = (left + right) / 2;
+		}
+
+		System.out.println(mid);
+	}
+
+	static double f(double x) {
+		return A * x + B * Math.sin(x) - C;
+	}
+
+}
