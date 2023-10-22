a64b4145a7d199e1b0d9eadaa56ed02642b58d11
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-22 14:23:49 +0900

    [Gold I] Title: 책 페이지, Time: 76 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Gold/1019. 책 페이지/책 페이지.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int[] count = new int[10]; // 각 숫자의 개수를 저장하는 배열
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		solve(1, n, 1);
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 10; ++i)
+			sb.append(count[i]).append(' ');
+		System.out.println(sb);
+	}
+
+	static void solve(int a, int b, int digit) {
+		// a를 증가시키며 1의 자리를 0으로 만듦
+		while (a % 10 != 0 && a <= b) {
+			calc(a, digit);
+			++a;
+		}
+
+		if (a > b)
+			return;
+
+		// b를 감소시키며 1의 자리를 9로 만듦
+		while (b % 10 != 9 && b >= a) {
+			calc(b, digit);
+			--b;
+		}
+
+		// b - a + 1 * 자릿수만큼 숫자 개수 더함
+		int cnt = b / 10 - a / 10 + 1;
+
+		for (int i = 0; i < 10; ++i)
+			count[i] += cnt * digit;
+
+		// 다음 자릿수 계산
+		solve(a / 10, b / 10, digit * 10);
+	}
+
+	static void calc(int n, int digit) {
+		while (n > 0) {
+			count[n % 10] += digit;
+			n /= 10;
+		}
+	}
+
+}
