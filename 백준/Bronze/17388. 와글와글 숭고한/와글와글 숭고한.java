f131c740d7a77ae782f28805d2bdfbcea1b186eb
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-10-28 19:51:59 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 76 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int S = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+
+		int sum = S + K + H;
+
+		if (sum >= 100) {
+			System.out.println("OK");
+		} else {
+			int min = Math.min(S, Math.min(K, H));
+
+			if (min == S) {
+				System.out.println("Soongsil");
+			} else if (min == K) {
+				System.out.println("Korea");
+			} else if (min == H) {
+				System.out.println("Hanyang");
+			}
+		}
+	}
+
+}
