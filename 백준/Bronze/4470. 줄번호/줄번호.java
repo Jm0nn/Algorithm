af3007b4dab384a8c450202f0cc023f65c1a6b89
Jm0nn <139559227+Jm0nn@users.noreply.github.com>
2023-11-15 08:55:35 +0900

    [Bronze IV] Title: 줄번호, Time: 72 ms, Memory: 11292 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; ++i)
+            sb.append(i).append(". ").append(br.readLine()).append('\n');
+        System.out.print(sb);
+    }
+}
