2fb93283fab5691ec1f2166149ec3ac629f5ec70
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2023-12-29 09:20:56 +0900

    [Bronze V] Title: SciComLove, Time: 72 ms, Memory: 11492 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0)
+            sb.append("SciComLove\n");
+        System.out.print(sb);
+    }
+}
