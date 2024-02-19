5cd6211de327bcd18bca19fe65487736ad60ea63
Jm0nn <kjm1994@naver.com>
2024-02-19 21:52:42 +0900

    Baekjoon 2029 성냥

---
+++ Algorithm/src/com/jm0nn/baekjoon/silver/s4/S4_2029_성냥.java
@@ -0,0 +1,59 @@
+package com.jm0nn.baekjoon.silver.s4;
+
+import java.io.*;
+
+public class S4_2029_성냥 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int L = 10;
+		char[][] matches = new char[L][L];
+
+		int cnt = 0;
+
+		for (int i = 0; i < L; ++i) {
+			String input = br.readLine();
+			for (int j = 0; j < L; ++j) {
+				char c = input.charAt(j);
+				matches[i][j] = input.charAt(j);
+				if (c == '-' || c == '|') {
+					++cnt;
+				}
+			}
+		}
+
+		int A = 24 - cnt / 2;
+		int B = 0;
+
+		for (int i = 0; i < L; ++i) {
+			for (int j = 0; j < L; ++j) {
+				if (matches[i][j] != '+') {
+					continue;
+				}
+
+				loop:
+				for (int s = 3; s <= 9; s += 3) {
+					for (int k = i; k <= i + s; ++k) {
+						for (int l = j; l <= j + s; ++l) {
+							if (k != i && k != i + s && l != j && l != j + s) {
+								continue;
+							} else if (k >= L || l >= L) {
+								break loop;
+							}
+
+							if (matches[k][l] == '.') {
+								continue loop;
+							}
+						}
+					}
+
+					++B;
+				}
+			}
+		}
+
+		System.out.println(A + " " + B);
+	}
+
+}
