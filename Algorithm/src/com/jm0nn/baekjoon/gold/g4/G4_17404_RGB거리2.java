91929153b999ca44a1f50345f0c1120a4a7d4f0e
김정민 <kjm1994@naver.com>
2024-02-21 17:07:03 +0900

    Baekjoon 17404 RGB 거리 2

---
+++ Algorithm/src/com/jm0nn/baekjoon/gold/g4/G4_17404_RGB거리2.java
@@ -0,0 +1,47 @@
+package com.jm0nn.baekjoon.gold.g4;
+
+import java.io.*;
+import java.util.*;
+
+public class G4_17404_RGB거리2 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] rgb = new int[n][3];
+		int[][] dp = new int[n][3];
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				rgb[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		final int INF = 1_111_111;
+		int ans = INF;
+
+		for (int k = 0; k < 3; ++k) {
+			for (int i = 0; i < 3; ++i) {
+				if (i == k) {
+					dp[0][i] = rgb[0][i];
+				} else {
+					dp[0][i] = INF;
+				}
+			}
+
+			for (int i = 1; i < n; ++i) {
+				dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
+				dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
+				dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
+			}
+
+			for (int i = 0; i < 3; ++i) {
+				if (i != k) {
+					ans = Math.min(ans, dp[n - 1][i]);
+				}
+			}
+		}
+
+		System.out.println(ans);
+	}
+}
