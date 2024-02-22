ca1bc1cae6a9112da8b114baee2f3eb467e0d10b
김정민 <kjm1994@naver.com>
2024-02-22 15:00:42 +0900

    Baekjoon 24078 Remainder

---
+++ Algorithm/src/com/jm0nn/baekjoon/bronze/b5/B5_24078_Remainder.java
@@ -0,0 +1,11 @@
+package com.jm0nn.baekjoon.bronze.b5;
+
+import java.io.*;
+
+public class B5_24078_Remainder {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		System.out.println(x % 21);
+	}
+}
