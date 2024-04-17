4299a7857f989d6aa8730c91ff9d9c14b052874b
김정민 <kjm1994@naver.com>
2024-04-17 09:34:56 +0900

    Baekjoon 1946 신입사원

---
+++ Algorithm/src/com/jm0nn/baekjoon/silver/s1/S1_1946_신입사원.java
@@ -0,0 +1,38 @@
+package com.jm0nn.baekjoon.silver.s1;
+
+import java.io.*;
+import java.util.*;
+
+public class S1_1946_신입사원 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			int[] score = new int[N + 1];
+
+			for (int i = 1; i <= N; ++i) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				score[a] = b;
+			}
+
+			int cnt = 0;
+			int min = N + 1;
+
+			for (int i = 1; i <= N; ++i) {
+				if (score[i] < min) {
+					++cnt;
+					min = score[i];
+				}
+			}
+
+			sb.append(cnt).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
