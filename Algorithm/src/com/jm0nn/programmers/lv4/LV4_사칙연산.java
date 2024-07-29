369ccda377b374bbe25546c7c579bddfc54168d4
Jm0nn <kjm1994@naver.com>
2024-07-29 20:10:40 +0900

    알고리즘

---
+++ Algorithm/src/com/jm0nn/programmers/lv4/LV4_사칙연산.java
@@ -0,0 +1,47 @@
+package com.jm0nn.programmers.lv4;
+
+public class LV4_사칙연산 {
+    public static void main(String[] args) {
+        LV4_사칙연산 lv4 = new LV4_사칙연산();
+        System.out.println(lv4.solution(new String[]{"1", "-", "3", "+", "5", "-", "8"})); // 1
+        System.out.println(lv4.solution(new String[]{"5", "-", "3", "+", "1", "+", "2", "-", "4"})); // 3
+    }
+
+    public int solution(String arr[]) {
+        int len = arr.length;
+
+        int[][] min = new int[len][len];
+        int[][] max = new int[len][len];
+
+        for (int i = 0; i < len; i += 2) {
+            for (int j = 0; j < len; j += 2) {
+                if (i == j) {
+                    int num = Integer.parseInt(arr[i]);
+                    min[i][j] = num;
+                    max[i][j] = num;
+                } else {
+                    min[i][j] = Integer.MAX_VALUE;
+                    max[i][j] = Integer.MIN_VALUE;
+                }
+            }
+        }
+
+        for (int size = 3; size <= len; size += 2) {
+            for (int k = 1; k <= len - 2; k += 2) {
+                for (int i = Math.max(k - (size - 2), 0); i <= Math.min(k - 1, len - size); i += 2) {
+                    int j = i + size - 1;
+
+                    if (arr[k].equals("+")) {
+                        min[i][j] = Math.min(min[i][j], min[i][k - 1] + min[k + 1][j]);
+                        max[i][j] = Math.max(max[i][j], max[i][k - 1] + max[k + 1][j]);
+                    } else {
+                        min[i][j] = Math.min(min[i][j], min[i][k - 1] - max[k + 1][j]);
+                        max[i][j] = Math.max(max[i][j], max[i][k - 1] - min[k + 1][j]);
+                    }
+                }
+            }
+        }
+
+        return max[0][len - 1];
+    }
+}
