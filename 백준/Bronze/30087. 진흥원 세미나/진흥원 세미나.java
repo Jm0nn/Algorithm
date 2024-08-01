bf7515a2ddaa99bd65e66b91fc40df150f672bd0
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-01 15:36:55 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 84 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Map<String, String> map = new HashMap<>();
+        map.put("Algorithm", "204\n");
+        map.put("DataAnalysis", "207\n");
+        map.put("ArtificialIntelligence", "302\n");
+        map.put("CyberSecurity", "B101\n");
+        map.put("Network", "303\n");
+        map.put("Startup", "501\n");
+        map.put("TestStrategy", "105\n");
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) sb.append(map.get(br.readLine()));
+        System.out.print(sb);
+    }
+}
