662c549cce571bab73bbec6e21dc2bc3b6d56e21
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-01 15:31:24 +0900

    [Bronze V] Title: 라면 공식, Time: 88 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            sb.append(a * (x  - 1) + b).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
