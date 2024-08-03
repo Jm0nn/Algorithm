803481302ba89ba89008c6f2c3556b3800c292d2
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-03 19:51:08 +0900

    [Bronze IV] Title: 시험 점수, Time: 68 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum1 = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; ++i) sum1 += Integer.parseInt(st.nextToken());
+        int sum2 = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; ++i) sum2 += Integer.parseInt(st.nextToken());
+        System.out.print(Math.max(sum1, sum2));
+    }
+}
