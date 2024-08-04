18046c1d566bf04cb92a309eb8dc61333817ecf1
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-04 15:49:03 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 64 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input;
+        while (!(input = br.readLine()).equals("TTYL")) sb.append(conv(input));
+        sb.append("talk to you later");
+        System.out.print(sb);
+    }
+
+    static String conv(String input) {
+        switch (input) {
+            case "CU":
+                return "see you\n";
+            case ":-)":
+                return "I’m happy\n";
+            case ":-(":
+                return "I’m unhappy\n";
+            case ";-)":
+                return "wink\n";
+            case ":-P":
+                return "stick out my tongue\n";
+            case "(~.~)":
+                return "sleepy\n";
+            case "TA":
+                return "totally awesome\n";
+            case "CCC":
+                return "Canadian Computing Competition\n";
+            case "CUZ":
+                return "because\n";
+            case "TY":
+                return "thank-you\n";
+            case "YW":
+                return "you’re welcome\n";
+            default:
+                return input + '\n';
+        }
+    }
+}
