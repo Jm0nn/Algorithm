0b9ff21be5f220d9a6404134d7791dfacc878b74
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-04 20:50:05 +0900

    [Bronze IV] Title: Koszykarz, Time: 364 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/8710. Koszykarz/Koszykarz.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        while (k < w) {
+            k += m;
+            ++cnt;
+        }
+        System.out.print(cnt);
+    }
+}
