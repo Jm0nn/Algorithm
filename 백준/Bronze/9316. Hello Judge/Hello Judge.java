3ee2e64fc88cf656e3bceb9a6f8b00b25dae6535
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-05 22:38:03 +0900

    [Bronze IV] Title: Hello Judge, Time: 60 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; ++i) sb.append("Hello World, Judge ").append(i).append("!\n");
+        System.out.print(sb);
+    }
+}
