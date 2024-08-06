b7d704af08b1c9342a9a213a096780c7080aa094
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-06 23:38:13 +0900

    [Bronze IV] Title: Communication Channels, Time: 60 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Bronze/11121. Communication Channels/Communication Channels.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String input = st.nextToken();
+            String output = st.nextToken();
+            if (input.equals(output)) sb.append("OK\n");
+            else sb.append("ERROR\n");
+        }
+        System.out.print(sb);
+    }
+}
