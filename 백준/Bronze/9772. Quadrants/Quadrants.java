0037ccd7a25edc9e5455935f804d34b86a5f6bd5
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-06 21:28:54 +0900

    [Bronze IV] Title: Quadrants, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            float x = Float.parseFloat(st.nextToken());
+            float y = Float.parseFloat(st.nextToken());
+
+            if (x > 0 && y > 0) sb.append("Q1\n");
+            else if (x < 0 && y > 0) sb.append("Q2\n");
+            else if (x < 0 && y < 0) sb.append("Q3\n");
+            else if (x > 0 && y < 0) sb.append("Q4\n");
+            else sb.append("AXIS\n");
+
+            if (x == 0 && y == 0) break;
+        }
+        System.out.print(sb);
+    }
+}
