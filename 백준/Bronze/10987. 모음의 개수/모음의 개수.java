88e039ddf092e651c9a66011c321c34fe4721a84
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-06 23:29:29 +0900

    [Bronze IV] Title: 모음의 개수, Time: 64 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Bronze/10987. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -3,10 +3,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] word = br.readLine().toCharArray();
+        String[] word = br.readLine().split("");
         String vowel = "aeiou";
         int cnt = 0;
-        for (char c : word) if (vowel.contains(String.valueOf(c))) ++cnt;
+        for (String s : word) if (vowel.contains(s)) ++cnt;
         System.out.print(cnt);
     }
 }
