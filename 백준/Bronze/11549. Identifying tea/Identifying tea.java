676c22ff57bb270c6deba7df0bb04ffc888d0642
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-07 11:59:31 +0900

    [Bronze IV] Title: Identifying tea, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/11549. Identifying tea/Identifying tea.java
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -1,9 +1,13 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char t = br.readLine().charAt(0);
-        System.out.print(br.readLine().chars().filter(c -> c == t).count());
+        String t = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < 5; ++i) if (t.equals(st.nextToken())) ++cnt;
+        System.out.print(cnt);
     }
 }
