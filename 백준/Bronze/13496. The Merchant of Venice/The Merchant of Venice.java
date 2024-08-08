acd2bae1f0acb4e31156604006c29d4c1ffa91d4
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-08 18:42:38 +0900

    [Bronze IV] Title: The Merchant of Venice, Time: 68 ms, Memory: 11872 KB -BaekjoonHub

---
+++ 백준/Bronze/13496. The Merchant of Venice/The Merchant of Venice.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int ds = 1; ds <= k; ++ds) {
+            sb.append("Data Set ").append(ds).append(":\n");
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+
+            for (int i = 0; i < n; ++i) {
+                st = new StringTokenizer(br.readLine());
+                int di = Integer.parseInt(st.nextToken());
+                int vi = Integer.parseInt(st.nextToken());
+
+                int day = di / s + (di % s > 0 ? 1 : 0);
+                if (day <= d) sum += vi;
+            }
+
+            sb.append(sum).append("\n\n");
+        }
+
+        System.out.print(sb);
+    }
+}
