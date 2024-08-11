45ac72fceb53dda8dd01036ee3c603837c1862f9
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-11 12:31:45 +0900

    [Bronze IV] Title: Every Second Counts, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/15080. Every Second Counts/Every Second Counts.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] time = new int[2][3];
+        for (int i = 0; i < 2; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " :");
+            for (int j = 0; j < 3; ++j) time[i][j] = Integer.parseInt(st.nextToken());
+        }
+        int second = (time[1][0] - time[0][0]) * 60 * 60
+                + (time[1][1] - time[0][1]) * 60
+                + time[1][2] - time[0][2];
+        if (second < 0) second += 24 * 60 * 60;
+        System.out.print(second);
+    }
+}
