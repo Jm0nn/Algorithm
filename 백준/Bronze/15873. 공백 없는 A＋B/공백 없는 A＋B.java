b37d7eb5295cced232c0860c615094a66d1c3d65
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-13 11:22:34 +0900

    [Bronze IV] Title: 공백 없는 A+B, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int len = input.length();
+        int a, b;
+        if (len == 2) {
+            a = Integer.parseInt(input.substring(0,1));
+            b = Integer.parseInt(input.substring(1));
+        } else if (len == 3) {
+            if (input.charAt(1) == '0') {
+                a = 10;
+                b = Integer.parseInt(input.substring(2));
+            } else {
+                a = Integer.parseInt(input.substring(0, 1));
+                b = 10;
+            }
+        } else {
+            a = b = 10;
+        }
+        System.out.print(a + b);
+    }
+}
