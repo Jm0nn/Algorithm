6f936de22ddaf9d3ce2f2b0fba37a1c17c0630a7
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-13 17:25:43 +0900

    [level 3] Title: N으로 표현, Time: 105.71 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,61 +1,32 @@
 import java.util.*;
 
 public class Solution {
+
+    private int N;
+    private Map<Integer, Integer> dp;
+
     public int solution(int N, int number) {
-        int answer = -1;
+        this.N = N;
+        dp = new HashMap<>();
 
-        final int MAX_NUM = 32000;
+        for (int i = N, digit = 1; i <= 1111111111; i = i * 10 + N, ++digit) dp.put(i, digit);
 
-        int[] dp = new int[MAX_NUM + 1];
+        recur(0, 0);
 
-        Arrays.fill(dp, 9);
-
-        int tmp = N;
-        for (int j = 1; j <= 8; ++j) {
-            if (tmp > MAX_NUM) break;
-
-            dp[tmp] = j;
-            tmp = tmp * 10 + N;
-        }
-
-        while (true) {
-            boolean flag = false;
-
-            for (int i = 1; i <= MAX_NUM; ++i) {
-                if (dp[i] == 9) continue;
-
-                for (int j = 1; j <= MAX_NUM; ++j) {
-                    if (dp[j] == 9) continue;
-
-                    int cnt = dp[i] + dp[j];
-
-                    if (cnt > 8) continue;
-
-                    if (i + j <= MAX_NUM && dp[i + j] > cnt) {
-                        dp[i + j] = cnt;
-                        flag = true;
-                    }
-
-                    if (i - j > 0 && dp[i - j] > cnt) {
-                        dp[i - j] = cnt;
-                        flag = true;
-                    }
-
-                    if (i * j <= MAX_NUM && dp[i * j] > cnt) {
-                        dp[i * j] = cnt;
-                        flag = true;
-                    }
-
-                    if (i / j > 0 && dp[i / j] > cnt) {
-                        dp[i / j] = cnt;
-                        flag = true;
-                    }
-                }
-            }
-
-            if (dp[number] < 9 || !flag) break;
-        }
-
-        return dp[number] < 9 ? dp[number] : -1;
+        return dp.getOrDefault(number, -1);
     }
+
+    private void recur(int cnt, int num) {
+        if (cnt > 8 || (cnt > 0 && num == 0)) return;
+
+        dp.put(num, Math.min(dp.getOrDefault(num, cnt), cnt));
+
+        for (int i = N, digit = 1; i < 1111111111; i = i * 10 + N, ++digit) {
+            recur(cnt + digit, num + i);
+            recur(cnt + digit, num - i);
+            recur(cnt + digit, num * i);
+            recur(cnt + digit, num / i);
+        }
+    }
+
 }
