5fb0b6212295fb9ab76c0a71bcb17045e11ad52d
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-14 14:27:42 +0900

    [Bronze IV] Title: Contemporary Art, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long a = Long.parseLong(br.readLine());
+        System.out.print(Math.sqrt(a) * 4);
+    }
+}
