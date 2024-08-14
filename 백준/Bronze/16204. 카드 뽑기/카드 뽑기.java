0cafd3637e61d04028b020f47ac292c8f2cc2fec
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-14 13:17:17 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int maxO = Math.min(m, k);
+        int maxX = Math.min(n - m, n - k);
+        System.out.print(maxO + maxX);
+    }
+}
