72dbbeda2daed812a0e0621de43994105b35b9dc
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-14 16:03:14 +0900

    [level 3] Title: N으로 표현, Time: 27.48 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,32 +1,31 @@
 import java.util.*;
 
 public class Solution {
-
-    private int N;
-    private Map<Integer, Integer> dp;
-
     public int solution(int N, int number) {
-        this.N = N;
-        dp = new HashMap<>();
+        Set<Integer>[] dp = new Set[9];
 
-        for (int i = N, digit = 1; i <= 1111111111; i = i * 10 + N, ++digit) dp.put(i, digit);
-
-        recur(0, 0);
-
-        return dp.getOrDefault(number, -1);
-    }
-
-    private void recur(int cnt, int num) {
-        if (cnt > 8 || (cnt > 0 && num == 0)) return;
-
-        dp.put(num, Math.min(dp.getOrDefault(num, cnt), cnt));
-
-        for (int i = N, digit = 1; i < 1111111111; i = i * 10 + N, ++digit) {
-            recur(cnt + digit, num + i);
-            recur(cnt + digit, num - i);
-            recur(cnt + digit, num * i);
-            recur(cnt + digit, num / i);
+        for (int i = N, digit = 1; digit <= 8; i = i * 10 + N, ++digit) {
+            if (i == number) return digit;
+            dp[digit] = new HashSet<>();
+            dp[digit].add(i);
         }
-    }
 
+        for (int i = 2; i <= 8; ++i) {
+            for (int j = 1; j < i; ++j) {
+                int k = i - j;
+                for (int n1 : dp[j]) {
+                    for (int n2 : dp[k]) {
+                        dp[i].add(n1 + n2);
+                        dp[i].add(n1 - n2);
+                        dp[i].add(n1 * n2);
+                        if (n2 != 0) dp[i].add(n1 / n2);
+                    }
+                }
+            }
+
+            if (dp[i].contains(number)) return i;
+        }
+
+        return -1;
+    }
 }
