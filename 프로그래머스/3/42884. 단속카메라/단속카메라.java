f17d51bfe9e873a885e01fbdda0a37e8f5719546
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-14 17:02:33 +0900

    [level 3] Title: 단속카메라, Time: 10.87 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+
+        Arrays.sort(routes, (o1, o2) -> o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1]);
+
+        int len = routes.length;
+
+        int pos = routes[0][1];
+
+        for (int i = 1; i < len; ++i) {
+            int left = routes[i][0];
+            int right = routes[i][1];
+
+            if (right <= pos) pos = right;
+            else if (left <= pos) continue;
+            else {
+                pos = right;
+                ++answer;
+            }
+        }
+
+        return answer;
+    }
+}
