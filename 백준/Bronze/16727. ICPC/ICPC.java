bf47eb6b0eab03ac6dff49083515d7b9fcc760a5
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-16 17:20:22 +0900

    [Bronze IV] Title: ICPC, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/16727. ICPC/ICPC.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int p1 = Integer.parseInt(st.nextToken());
+        int s1 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int s2 = Integer.parseInt(st.nextToken());
+        int p2 = Integer.parseInt(st.nextToken());
+        int p = p1 + p2;
+        int s = s1 + s2;
+        String answer;
+        if (p == s) {
+            if (p1 == s2) answer = "Penalty";
+            else if  (p1 > s2) answer = "Esteghlal";
+            else answer = "Persepolis";
+        } else if (p > s) {
+            answer = "Persepolis";
+        } else {
+            answer = "Esteghlal";
+        }
+        System.out.print(answer);
+    }
+}
