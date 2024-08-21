5075bd4104ac0721797ca0344e3d7e8c72696d4d
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-21 16:32:05 +0900

    [level 4] Title: 징검다리, Time: 38.14 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+
+        int len = rocks.length + 1;
+
+        int[] tmpRocks = new int[len];
+        System.arraycopy(rocks, 0, tmpRocks, 0, len - 1);
+        tmpRocks[len - 1] = distance;
+        rocks = tmpRocks;
+
+        Arrays.sort(rocks);
+
+        int left = 0;
+        int right = distance;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            int cur = 0;
+            int cnt = 0;
+
+            for (int rock : rocks) {
+                if (rock - cur < mid) ++cnt;
+                else cur = rock;
+            }
+
+            if (cnt > n) {
+                right = mid - 1;
+            } else {
+                answer = mid;
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+}
