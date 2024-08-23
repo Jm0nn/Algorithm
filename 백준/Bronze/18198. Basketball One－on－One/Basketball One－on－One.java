d38993fb0a078fefd960179a2abec8742bcf6edb
Kim Jeongmin <139559227+Jm0nn@users.noreply.github.com>
2024-08-23 11:50:27 +0900

    [Bronze IV] Title: Basketball One-on-One, Time: 64 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/18198. Basketball One－on－One/Basketball One－on－One.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input = br.readLine().toCharArray();
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < input.length; i += 2) {
+            if (input[i] == 'A') a += input[i + 1] - '0';
+            else b += input[i + 1] - '0';
+        }
+        System.out.print(a > b ? 'A' : 'B');
+    }
+}
